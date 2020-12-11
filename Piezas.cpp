45207643f7a3e560bb90025ca6bb3baea892c1ef
Chase May <cmay11@mail.csuchico.edu>
2020-12-11 15:23:18 -0800

    Created the game and have done extensive testing

--- Piezas.cpp
+++ Piezas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -17,32 +17,73 @@
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+  board.resize(BOARD_ROWS);
+  for(int row = 0; row < BOARD_ROWS; row++){
+    board[row].resize(BOARD_COLS);
+    for(int column = 0; column < BOARD_COLS; column++){
+      board[row][column] = Blank;
+    }
+  }
+  //X's turn first
+  turn = X;
 }
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
 **/
+
+//The board should already be the same size so just set every spot to Blank.
 void Piezas::reset()
 {
+  for(int row = 0; row < BOARD_ROWS; row++){
+    for(int column = 0; column < BOARD_COLS; column++){
+      board[row][column] = Blank;
+    }
+  }
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
+    Piece curPiece = turn;
+
+//Check to make sure X goes first and switch after
+  if(turn == O){
+    turn = X;
+  }
+  else if(turn == X){
+    turn = O;
+  }
+
+//Check for out of bounds and loses the player's turn
+  if(column < 0)
+  {
+    return Invalid;
+  }
+
+//Place curPiece and return it
+    for(int row = 0; row < BOARD_ROWS; row++){
+      if(board[row][column] == Blank){
+        board[row][column] = curPiece;
+        return curPiece;
+      }
+    }
+
     return Blank;
+
 }
 
 /**
@@ -51,13 +92,19 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+//Checking to see if pieceAt is out of bounds.
+  if(row < 0 || row > BOARD_ROWS || row > 4 || column < 0 || column > BOARD_COLS || column > 4){
+    return Invalid;
+  }
+  else{
+    return board[row][column];
+  }
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
@@ -65,5 +112,50 @@ Piece Piezas::pieceAt(int row, int column)
 **/
 Piece Piezas::gameState()
 {
+    int xCurPiece = 0;
+    int oCurPiece = 0;
+    int xHighPiece = 0;
+    int oHighPiece = 0;
+    bool isOver = true;
+
+    //Check if game is over
+  for(int row = 0; row < BOARD_ROWS; row++){
+    for(int column = 0; column < BOARD_COLS; column++){
+      if(board[row][column] == Blank){
+        isOver = false;
+      }
+    }
+  }
+  if(isOver == false){
+    return Invalid;
+  }
+
+  for(int row = 0; row < BOARD_ROWS; row++){
+    for(int column = 0; column < BOARD_COLS; column++){
+      if(board[row][column] == X){
+        xCurPiece += 1;
+        if(xCurPiece > xHighPiece){
+          xHighPiece = xCurPiece;
+        }
+        oHighPiece = 0;
+      }
+      else if(board[row][column] == O){
+        oCurPiece += 1;
+        if(oCurPiece > oHighPiece){
+          oHighPiece = oCurPiece;
+        }
+        xHighPiece = 0;
+      }
+    }
+  }
+
+  if(xHighPiece > oHighPiece){
+    return X;
+  }
+  else if(oHighPiece > xHighPiece){
+    return O;
+  }
+  else{
     return Blank;
+  }
 }
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,127 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
 		virtual ~PiezasTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PiezasTest, sanityCheck)
+TEST(PiezasTest, simpleMoveCheck)
 {
-	ASSERT_TRUE(true);
+	Piezas board;
+  Piece piece = board.dropPiece(1);
+  ASSERT_EQ(piece, X);
+}
+
+TEST(PiezasTest, simepleNegativeCheck)
+{
+	Piezas board;
+  Piece piece = board.dropPiece(-1);
+  ASSERT_EQ(piece, Invalid);
+}
+
+TEST(PiezasTest, simepleOutBoundsCheck)
+{
+	Piezas board;
+  Piece piece = board.dropPiece(12);
+  ASSERT_EQ(piece, Blank);
+}
+
+TEST(PiezasTest, xInBoundsCheck)
+{
+	Piezas board;
+  board.dropPiece(1);
+  ASSERT_EQ(board.pieceAt(0,1), X);
+}
+
+TEST(PiezasTest, oInBoundsCheck)
+{
+	Piezas board;
+  board.dropPiece(1);
+  board.dropPiece(2);
+  ASSERT_EQ(board.pieceAt(0,2), O);
+}
+
+TEST(PiezasTest, xGoesFirstCheck)
+{
+	Piezas board;
+  ASSERT_EQ(board.dropPiece(1), X);
+}
+
+TEST(PiezasTest, oGoesSecondCheck)
+{
+	Piezas board;
+  ASSERT_EQ(board.dropPiece(1), X);
+  ASSERT_EQ(board.dropPiece(2), O);
+}
+
+TEST(PiezasTest, xGoesThirdCheck)
+{
+	Piezas board;
+  ASSERT_EQ(board.dropPiece(1), X);
+  ASSERT_EQ(board.dropPiece(2), O);
+  ASSERT_EQ(board.dropPiece(3), X);
+}
+
+TEST(PiezasTest, oGoesFourthCheck)
+{
+	Piezas board;
+  ASSERT_EQ(board.dropPiece(1), X);
+  ASSERT_EQ(board.dropPiece(2), O);
+  ASSERT_EQ(board.dropPiece(3), X);
+  ASSERT_EQ(board.dropPiece(2), O);
+}
+
+TEST(PiezasTest, resetFunctionCheck)
+{
+	Piezas board;
+  board.dropPiece(1);
+  board.reset();
+  ASSERT_EQ(board.pieceAt(0, 1), Blank);
+}
+
+TEST(PiezasTest, xWins)
+{
+	Piezas board;
+  board.dropPiece(0);//X
+  board.dropPiece(0);//O
+  board.dropPiece(1);
+  board.dropPiece(2);
+  board.dropPiece(0);
+  board.dropPiece(1);
+  board.dropPiece(1);
+  board.dropPiece(3);
+  board.dropPiece(3);
+  board.dropPiece(2);
+  board.dropPiece(1);
+  board.dropPiece(0);
+  board.dropPiece(2);
+  board.dropPiece(0);
+  board.dropPiece(3);
+  ASSERT_EQ(board.gameState(), X);
+}
+
+TEST(PiezasTest, oWins)
+{
+	Piezas board;
+  board.dropPiece(1);
+  board.dropPiece(0);
+  board.dropPiece(2);
+  board.dropPiece(0);
+  board.dropPiece(3);
+  board.dropPiece(0);
+  board.dropPiece(1);
+  board.dropPiece(1);
+  board.dropPiece(3);
+  board.dropPiece(2);
+  board.dropPiece(0);
+  board.dropPiece(2);
+  board.dropPiece(0);
+  board.dropPiece(3);
+  ASSERT_EQ(board.gameState(), O);
 }
